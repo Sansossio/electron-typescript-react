3196349f416c063aa963e4c55baa28938937e7db
Sansossio <juliosansossio@gmail.com>
2020-04-14 19:05:06 +0200

    move base url logic to a file

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,8 @@
-import * as isDev from 'electron-is-dev'
 import { BrowserWindow, app } from 'electron'
 import * as dotenv from 'dotenv'
-import { format } from 'url'
-import { join } from 'path'
+import { baseUrl } from './url/base-url'
 
 dotenv.config()
-
-export function baseUrl () {
-  if (isDev) {
-    return 'http://localhost:3000'
-  }
-  return format({
-    pathname: (join(__dirname, '..', 'build', 'index.html')),
-    protocol: 'file',
-    slashes: true
-  })
-}
 
 app.on('ready', () => {
   const main = new BrowserWindow({
